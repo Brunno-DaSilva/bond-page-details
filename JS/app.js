840c037cf900056a23ab526285ce09c88a461b85
Bruno DaSilva <hi.dasilvab@gmail.com>
2020-10-30 20:05:48 -0500

    working on the mobile version

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,14 @@
 console.log("Ciao Bella, me piacere parlare italiano");
+const top_Container = document.getElementById("container__top");
+
+var mySwiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
